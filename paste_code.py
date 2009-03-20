54a36d0326a429cdad852156de522171b040cc04
Gabriel <contact@huau-gabriel.fr>
2009-03-20 03:46:51 +0100

    add file options parsing

--- paste_code.py
+++ paste_code.py
@@ -1,12 +1,25 @@
-import httplib, urllib, sys, webbrowser
+import httplib, urllib, sys, webbrowser, os
+from optparse import OptionParser
+#import pygtk
+#pygtk.require('2.0')
+#import gtk
 
 # Verification des arguments
-if len(sys.argv) != 2:
-	print "error with argument size"
-	exit(1)
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename",
+                  help="file to be paste on rafb.net", metavar="FILE")
+(options, args) = parser.parse_args()
+
+if options.filename == None:
+	print "Aucun fichier en entree"
+	exit(0)
+
 
 # Lecture du fichier a paster
-ofi = open(sys.argv[1], 'r')
+#clipboard = gtk.clipboard_get()
+#text = clipboard.wait_for_text()
+#print text
+ofi = open(options.filename, 'r')
 buffer = ofi.read()
 
 type = "C++"
@@ -21,7 +34,7 @@ conn.request("POST", "/paste/paste.php", params, headers)
 
 # Recupere et affiche le resultat
 response = conn.getresponse()
-if response.status != 302
+if response.status != 302:
 	print "Format de la reponse non gere !"
 	exit(1)
 print response.msg.values()[4]
